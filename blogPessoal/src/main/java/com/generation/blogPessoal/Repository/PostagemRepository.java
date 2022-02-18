95de360708703d522618fe1959e596cce9d05b6f
Keyla <Keyladias.dk@hotmail.com>
2022-02-18 19:15:24 -0300

    Blog Pessoal

--- blogPessoal/src/main/java/com/generation/blogPessoal/Repository/PostagemRepository.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/Repository/PostagemRepository.java
@@ -1,11 +1,17 @@
 package com.generation.blogPessoal.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogPessoal.model.Postagem;
 
+
+
+
 @Repository
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
 
+	List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
--- blogPessoal/src/main/java/com/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/controller/PostagemController.java
@@ -2,10 +2,17 @@ package com.generation.blogPessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +34,41 @@ public class PostagemController {
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById (@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+		
+	}
 	
-	 }
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));	
+		
+	}
+	@PutMapping
+    public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
+        return postagemRepository.findById(postagem.getId())
+                .map(resposta -> ResponseEntity.ok(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletePostagem(@PathVariable Long id) {
+        return postagemRepository.findById(id)
+                .map(resposta -> {
+                    postagemRepository.deleteById(id);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        })
+                .orElse(ResponseEntity.notFound().build());
+
+
+	}	
+	
+}
+	
+	
+	 
 
 
