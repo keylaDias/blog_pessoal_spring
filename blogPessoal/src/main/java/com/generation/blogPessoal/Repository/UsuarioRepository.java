ff2a045d3961426742cb086185f155a5a143a332
Keyla <Keyladias.dk@hotmail.com>
2022-03-04 18:35:22 -0300

    blog Pessoal

---
+++ blogPessoal/src/main/java/com/generation/blogPessoal/Repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package com.generation.blogPessoal.Repository;
+
+import java.util.Optional;
+
+import com.generation.blogPessoal.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	/**
+	 * Método que busca um usuário pelo seu usuario (email).
+	 * 
+	 * select * from tb_usuarios where usuario = "usuario procurado"
+	 */
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
